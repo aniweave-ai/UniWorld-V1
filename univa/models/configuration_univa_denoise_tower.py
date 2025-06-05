31869e9f8420bdd6615cc101c93ef0322138a0dd
LinB203 <2267330597@qq.com>
2025-06-05 16:19:19 +0800

    Fix bug in attn autoset

--- univa/models/configuration_univa_denoise_tower.py
+++ univa/models/configuration_univa_denoise_tower.py
@@ -18,6 +18,7 @@ class UnivaDenoiseTowerConfig(PretrainedConfig):
         **kwargs,
     ):
         super().__init__(**kwargs)
+        self._attn_implementation_autoset = True
 
         self.denoiser_type = denoiser_type
         self.denoise_projector_type = denoise_projector_type
